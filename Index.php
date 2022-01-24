8cecf80d3201e09ec1e155b2870e015ac067ced6
srdjanpetkovski <srdjanpetkovski@gmail.com>
2022-01-24 22:08:26 +0100

    header css

---
+++ Index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Creative Design Studio</title>
+</head>
+<body>
+
+    <!-- nav -->
+    <nav>
+        <div class="container">
+            <a class="logo" href="index.php">Creative</a>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="team.php">Team</a></li>
+                <li><a href="projects.php">Projects</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <!-- nav -->
+    <header>
+        <div>
+            <hgroup>
+                <h1>Creative</h1>
+                <h3>Design Studio</h3>
+                <h5>Web & Graphic design</h5>
+            </hgroup>
+        </div>
+    </header>
+
+
+</body>
+</html>
